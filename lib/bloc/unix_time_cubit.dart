0bf25f8b2bf6bbb4d2202d3d09dd7c900a68e079
Franklin Diaz <franklin83diaz@gmail.com>
2023-12-22 21:58:01 -0500

    fixed page

--- lib/bloc/unix_time_cubit.dart
+++ lib/bloc/unix_time_cubit.dart
@@ -3,15 +3,17 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class UnixtimeCubit extends Cubit<int> {
   UnixtimeCubit() : super(0);
 
-  void increment() => emit(state + 1);
-  void decrement() => emit(state - 1);
-
   void reset() => emit(0);
 
+  bool runing = false;
+
   void start() async {
+    if (runing) return;
+    runing = true;
     for (int i = 0; i < (25 * 60); i++) {
       await Future.delayed(const Duration(seconds: 1));
       emit(state + 1);
     }
+    runing = false;
   }
 }
--- lib/pages/home_Page.dart
+++ lib/pages/home_Page.dart
@@ -3,8 +3,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pomodoro_timer/bloc/unix_time_cubit.dart';
 import 'package:pomodoro_timer/utils/convert_time.dart';
 
-class HomePsge extends StatelessWidget {
-  const HomePsge({super.key});
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -12,29 +12,38 @@ class HomePsge extends StatelessWidget {
       create: (_) => UnixtimeCubit(),
       child: Scaffold(
         body: Center(
-          child: Center(
-              child: Column(
-            children: [
-              BlocBuilder<UnixtimeCubit, int>(
-                builder: (context, timer) {
-                  String timerString = convertTime(timer);
-                  return Column(
-                    children: [
-                      Text(
+          child: Center(child: BlocBuilder<UnixtimeCubit, int>(
+            builder: (context, timer) {
+              String timerString = convertTime(timer);
+              return Column(
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: Center(
+                      child: Text(
                         timerString,
                         style: const TextStyle(
                             fontSize: 60, color: Colors.deepPurple),
                       ),
-                      TextButton(
-                          onPressed: () {
-                            context.read<UnixtimeCubit>().start();
-                          },
-                          child: const Text('Start')),
-                    ],
-                  );
-                },
-              ),
-            ],
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Column(
+                      children: [
+                        const Spacer(),
+                        TextButton(
+                            onPressed: () {
+                              context.read<UnixtimeCubit>().start();
+                            },
+                            child: const Text('Start')),
+                        const Spacer(),
+                      ],
+                    ),
+                  ),
+                ],
+              );
+            },
           )),
         ),
       ),
